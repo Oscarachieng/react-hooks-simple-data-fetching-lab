93027b629e3337599cb38b6197f0bbad9e082bd1
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-07-19 10:28:27 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+
+import React, { useState , useEffect } from "react";
+ 
+function App () {
+   const [ image, setImage] = useState ([]);
+  //  const [ isLoaded, setIsLoaded] = useState(false)
+
+   useEffect ( () => {
+       fetch ("https://dog.ceo/api/breeds/image/random")
+       .then(response => response.json())
+       .then(data => {
+           console.log(data)
+           setImage(data.message)
+          //  setIsLoaded(true)
+       })
+   }, []);
+
+   if (!image) {
+    return <h2>Loading...</h2>
+   }
+
+   return (
+       
+      <>  
+        <p>Oscar</p>
+        <img src={image} alt = "My dog"/>
+      </>
+   )
+  }
+ 
+export default App;
